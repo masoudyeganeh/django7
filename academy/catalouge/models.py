fdece6d4d55ed7202b468e5569aa2076f6536de0
masoudyeganeh <50057163+masoudyeganeh@users.noreply.github.com>
2024-05-17 23:56:24 +0330

    struct-12

--- academy/catalouge/models.py
+++ academy/catalouge/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class ProductType(models.Model):
     title = models.CharField(max_length=35, blank=True, null=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
