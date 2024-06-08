ed731394322aad4e14f69ae2781a5eac5a66c06e
masoudyeganeh <50057163+masoudyeganeh@users.noreply.github.com>
2024-06-08 23:22:06 +0330

    struct-3

---
+++ academy/transaction/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Transaction(models.Model):
+    CHARGE = 1
+    PURCHASE = 2
+    TRANSFER = 3
+
+    transaction_type_choices = (
+        (CHARGE,'CHARGE'),
+        (PURCHASE, 'PURCHASE'),
+        (TRANSFER, 'TRANSFER')
+    )
+
+    user = models.ForeignKey(User, related_name='transactions', on_delete=models.RESTRICT)
+    transaction_type = models.PositiveSmallIntegerField(choices=transaction_type_choices, default=CHARGE)
+    amount = models.BigIntegerField()
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.transaction_type} - {self.amount}'
