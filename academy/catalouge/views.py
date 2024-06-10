f6f2ce6560c957cc016fb0bc21b475f4cb8e7b50
m.Yeganeh <masoudyeganeh92@gmail.com>
2024-06-10 16:33:32 +0330

    14030322-1

--- academy/catalouge/views.py
+++ academy/catalouge/views.py
@@ -1,5 +1,12 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalouge.models import Product, Brand
 
-def post_list(request):
-    return HttpResponse("show post list here")
+
+def product_list(request):
+    products = Product.objects.filter(Q(id__in=[1]) | Q(brand_id=4)).filter(product_type_id=1)
+    # p1 = Brand(name= "bmw")
+    # p1.name = "toyota"
+    # p1.save()
+    return HttpResponse("\n".join([f"{p.upc}" for p in products]))
